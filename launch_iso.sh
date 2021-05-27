81aeb3a1b2d9ccbf61b3fa518c59ce98999d18fc
Schimmenti <schimmenti@slurm.lab-ia.local>
2021-05-27 19:35:10 +0200

    Definitive

--- launch_iso.sh
+++ launch_iso.sh
@@ -2,26 +2,17 @@
 #SBATCH -v
 #SBATCH -N 1
 #SBATCH -c 2
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:0
 #SBATCH --mail-type=FAIL,END
 #SBATCH --mail-user=vincenzo.schimmenti@universite-paris-saclay.fr
 #SBATCH --job-name=isostress
+#SBATCH --time=12:00:00
+#SBTAHC --mem-per-cpu=10G
+eval $1 ; eval $2 ; eval $3 ; eval $4 ;
 
-
-eval $1 ; eval $2 ; eval $3 ;
-
-filename=/mnt/beegfs/home/schimmenti/corrQuake/viscoelastic/$source
+source_folder=/mnt/beegfs/home/schimmenti/corrQuake/viscoelastic/$folder
+output_folder=/mnt/beegfs/home/schimmenti/corrQuake/viscoelastic/$outfolder
+file_source=/mnt/beegfs/home/schimmenti/corrQuake/viscoelastic/$source
 program_src=/mnt/beegfs/home/schimmenti/corrQuake/viscoelastic/visco.out
-#srun "visco.out" "k0=0.06" "k1=0.0" "k2=1.0" "Lx=512" "Ly=512" "source_file=${filename}"
 
-n=1
-while read line; do
-# reading each line
-echo "Index :  $n : Name : $line"
-isosource=/mnt/beegfs/home/schimmenti/corrQuake/viscoelastic/$folder/events0_$line.dat
-isoout=/mnt/beegfs/home/schimmenti/corrQuake/viscoelastic/vdep_data/distr0_$line.dat
-echo $isosource
-srun "visco.out" "k0=0.06" "k1=0.0" "k2=1.0" "Lx=512" "Ly=512" "iso_stress_file=${isosource}" "iso_output_file=${isoout}" "iso_stress_count=3000"
-sleep 1
-n=$((n+1))
-done < $filename
+$program_src k0=0.06 k1=0.0 k2=1.0 Lx=512 Ly=512 source_file=$file_source iso_source_folder=$source_folder iso_output_folder=$output_folder iso_stress_count=3000 iso_resume_from=$resume
